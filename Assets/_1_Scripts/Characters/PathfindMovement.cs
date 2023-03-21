670cb51c9b07936872d238b230bee2928f51fdfb
Ingaspio <112221234+Ingaspio@users.noreply.github.com>
2023-03-21 20:05:06 +0900

    Pathfinding
    Improved pathfinding alilabit, todolist saves and scenechange algorithm, then all gonna be smooth

--- Assets/_1_Scripts/Characters/PathfindMovement.cs
+++ Assets/_1_Scripts/Characters/PathfindMovement.cs
@@ -7,41 +7,59 @@ using UnityEngine.Windows;
 using DG.Tweening;
 using Input = UnityEngine.Input;
 using UnityEditor.UIElements;
+using System.Linq;
 
 public class PathfindMovement : MonoBehaviour
 {
-    //[SerializeField]
-    //Tilemap walls;
-    //[SerializeField]
-    //LineRenderer linePath;
-    List<Vector3> wayPoints;
-    private void Start()
-    {
-        wayPoints = new List<Vector3>();
-
-    }
+    List<Vector3> wayPoints = new List<Vector3>();
+    int tapTime = 0;
+    
     public void PathfindMove() 
     {
         Transform startPos = FindObjectOfType<PlayerCharacter>().transform;
         Transform endPos = FindObjectOfType<Destination>().transform;
         LineRenderer linePath = FindObjectOfType<LineRenderer>();
         Tilemap walls = GameObject.Find("Walls").GetComponent<Tilemap>();
-        if (Input.GetMouseButtonDown(0))
+        Tilemap floor = GameObject.Find("Floor").GetComponent<Tilemap>();
+        Tilemap fow = GameObject.Find("FogOfWar").GetComponent<Tilemap>();
+        if (Input.GetMouseButtonDown(0)) 
         {
             var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
-            endPos.position = mousePos;
-            wayPoints = AStar.FindPathClosest(walls, startPos.position, endPos.position);
-            if (wayPoints != null)
+            Vector3Int gridPosition = floor.WorldToCell(mousePos);
+            if (fow.HasTile(gridPosition)) { }
+            else if (floor.HasTile(gridPosition)) 
             {
-                for (int i = 0; i < wayPoints.Count; i++)
+                tapTime++;
+                if (tapTime == 1)
                 {
-                    //Debug.Log("Point: " + i + ", position: " + wayPoints[i]);
-                    linePath.positionCount = wayPoints.Count;
-                    linePath.SetPositions(wayPoints.ToArray());
-                    startPos.DOPath(wayPoints.ToArray(),1f);
+                    endPos.position = mousePos;
+                    wayPoints = AStar.FindPathClosest(walls, startPos.position, endPos.position);
+                    if (wayPoints != null)
+                    {
+                        for (int i = 0; i < wayPoints.Count; i++)
+                        {
+                            linePath.positionCount = wayPoints.Count;
+                            linePath.SetPositions(wayPoints.ToArray());
+                        }
+                    }
+                    else { startPos.DOComplete(); }
                 }
+                if (tapTime == 2 && wayPoints != null && Vector3.Distance(wayPoints.LastOrDefault(), mousePos) < 0.1f)
+                {
+                    Debug.Log("Last Vector: " + wayPoints.LastOrDefault());
+                    startPos.DOPath(wayPoints.ToArray(), 1f);
+                }
+                if (tapTime >= 2)
+                    tapTime = 0;
             }
+            
         }
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            wayPoints.Clear();
+            tapTime = 0;
+        }
+            
     }
 }
